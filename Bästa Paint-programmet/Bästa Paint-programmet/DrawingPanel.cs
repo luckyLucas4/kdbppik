ab3ded3a3c911d76966b85c2363a1ca822be5db7
Lucas Granberg <33953582+luckyLucas4@users.noreply.github.com>
2019-02-12 13:07:46 +0100

    Skapade panel-klassen

---
+++ Bästa Paint-programmet/Bästa Paint-programmet/DrawingPanel.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Bästa_Paint_programmet
+{
+    class DrawingPanel : Panel
+    {
+        private Point startPos;
+        private Point currentPos;
+        private bool drawing;
+        private List<Rectangle> rectangles = new List<Rectangle>();
+        private List<Rectangle> ellipses = new List<Rectangle>();
+
+        public string currentShape;
+
+        public DrawingPanel(string shape, Point position)
+        {
+            currentShape = shape;
+            BackColor = Color.Azure;
+            this.Location = position;
+            this.Size = new Size(200, 200);
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        public DrawingPanel(string shape, Point position, Size size)
+        {
+            currentShape = shape;
+            BackColor = Color.Azure;
+            this.Location = position;
+            this.Size = size;
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        protected Rectangle GetRectangle()
+        {
+            return new Rectangle(
+                Math.Min(startPos.X, currentPos.X),
+                Math.Min(startPos.Y, currentPos.Y),
+                Math.Abs(startPos.X - currentPos.X),
+                Math.Abs(startPos.Y - currentPos.Y)
+                );
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            currentPos = startPos = e.Location;
+            drawing = true;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            currentPos = e.Location;
+            if (drawing)
+            {
+                this.Invalidate();
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            switch (currentShape)
+            {
+                case "rectangle":
+                    AddRectangle(e);
+                    break;
+
+                case "circle":
+                    AddCircle(e);
+                    break;
+
+                case "pen":
+                    break;
+
+
+            }
+        }
+
+        private void AddRectangle(MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+                currentPos = e.Location;
+                var rc = GetRectangle();
+
+                if (rc.Width > 0 & rc.Height > 0)
+                {
+                    rectangles.Add(rc);
+                }
+
+                this.Invalidate(); // Rita om fönstret
+            }
+        }
+
+        private void AddCircle(MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+                currentPos = e.Location;
+                var rc = GetRectangle();
+
+                if (rc.Width > 0 & rc.Height > 0)
+                {
+                    ellipses.Add(rc);
+                }
+
+                this.Invalidate(); // Rita om fönstret
+            }
+        }
+
+        private void AddPen(MouseEventArgs e)
+        {
+
+        }
+
+        private void DrawRectangle(PaintEventArgs e)
+        {
+            if (rectangles.Count > 0)
+            {
+                e.Graphics.DrawRectangles(Pens.Black, rectangles.ToArray());
+            }
+
+            if (drawing)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRectangle());
+            }
+        }
+
+        private void DrawCircle(PaintEventArgs e)
+        {
+            if (ellipses.Count > 0)
+            {
+                foreach (Rectangle r in ellipses)
+                {
+                    e.Graphics.DrawEllipse(Pens.Black, r);
+                }
+            }
+
+            if (drawing)
+            {
+                e.Graphics.DrawEllipse(Pens.Red, GetRectangle());
+            }
+        }
+
+        
+    }
+}
--- Bästa Paint-programmet/Bästa Paint-programmet/Form1.Designer.cs
+++ Bästa Paint-programmet/Bästa Paint-programmet/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.Penna = new System.Windows.Forms.RadioButton();
             this.Linje = new System.Windows.Forms.RadioButton();
             this.Rektangel = new System.Windows.Forms.RadioButton();
             this.Cirkel = new System.Windows.Forms.RadioButton();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,6 +60,22 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Egenskaper";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(7, 69);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
             // 
             // Penna
             // 
@@ -106,22 +122,6 @@
             this.Cirkel.UseVisualStyleBackColor = true;
             this.Cirkel.CheckedChanged += new System.EventHandler(this.Cirkel_CheckedChanged);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(7, 38);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(7, 69);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 5;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -134,7 +134,7 @@
             this.Controls.Add(this.Penna);
             this.Controls.Add(this.Linje);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Bästa Paint Programmet";
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
